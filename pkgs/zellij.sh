aed357987cef3be186160451beaf0df0b5de5609
Lucas Campos Vieira <contato@lukerops.com>
2024-11-11 11:14:29 -0300

    adiciona script para instalar o zellij

---
+++ pkgs/zellij.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/sh
+
+PKGNAME='zellij'
+VERSION='0.41.1'
+URL="https://github.com/zellij-org/zellij/releases/download/v$VERSION/zellij-x86_64-unknown-linux-musl.tar.gz"
+
+curl --silent -L "$URL" | tar -xz -C "$TMPDIR"
+mv "$TMPDIR"/zellij "$HOME/.local/bin/"
