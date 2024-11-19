e1c60d474a09d63e97f5d52b442a2ff82bb26335
Lucas Campos Vieira <contato@lukerops.com>
2024-11-19 19:35:37 -0300

    ajusta instalação

--- install.sh
+++ install.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/sh
 
+mkdir -p "$HOME/.local/bin"
+
 for pkg in pkgs/*; do
     echo "Installing $(basename $pkg .sh)..."
     TMPDIR=$(mktemp -d)
-    
+
     . $pkg
-    
+    chmod +x "$HOME/.local/bin/$PKGNAME"
+
     rm -rf $TMPDIR
 done
--- pkgs/zellij.sh
+++ pkgs/zellij.sh
@@ -5,4 +5,4 @@ VERSION='0.41.1'
 URL="https://github.com/zellij-org/zellij/releases/download/v$VERSION/zellij-x86_64-unknown-linux-musl.tar.gz"
 
 curl --silent -L "$URL" | tar -xz -C "$TMPDIR"
-mv "$TMPDIR"/zellij "$HOME/.local/bin/"
+mv "$TMPDIR/zellij" "$HOME/.local/bin/"
